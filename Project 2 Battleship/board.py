bc6b4bdf5076bf9c86b3289ac6d383d816406328
James OKane <james@jenako.com.au>
2016-11-22 21:31:41 +1000

    Board setup

--- Project 2 Battleship/board.py
+++ Project 2 Battleship/board.py
@@ -46,78 +46,9 @@ class Board:
         self.make_board()
         self.print_board()
 
-# --------------------------------------
-
-# -- SETOUT PLAYER BOARD --
-
-    # Player input for ship position
-    def ship_position(self):
-        self.ship_input = (
-        (input("Where would you like to place the {}? The {} takes up {} spaces: ".format(
-            self.ship[0],
-            self.ship[0],
-            self.ship[1])
-            )).strip()).lower()
-        self.ship_loc = list(self.ship_input)
-        if len(self.ship_loc) == 2:
-            self.ship_x_pos = ord(self.ship_loc[1]) - 97
-            self.ship_y_pos = int(self.ship_input[0]) - 1
-        elif len(self.ship_loc) == 3:
-            self.ship_x_pos = ord(self.ship_loc[2]) - 97
-            self.ship_y_pos = int(self.ship_input[0] + self.ship_input[1]) - 1
-        else:
-            self.ship_position()
-
-
-    # Player input ship alignment
-    def ship_alignment(self):
-        self.alignment = (input("Press [h] to place it horizontal, or [v] to place it vertical: ")).lower()
-        if self.alignment == 'h':
-            # Check and see if the ship will fit on the board horizontally
-            for pos in range(int(self.ship[1])):
-                # If the ship fits add the markers to the board
-                if (self.ship_x_pos + int(self.ship[1])) > BOARD_SIZE:
-                    print(
-                    "The {} takes up {} spaces. There are only {} spaces available to the right of position {}"
-                        .format( self.ship[0], self.ship[1], (BOARD_SIZE - self.ship_x_pos), self.ship_input))
-                    print("Please try again")
-                    self.ship_position()
-                else:
-                    self.board[self.ship_y_pos][self.ship_x_pos + pos] = HORIZONTAL_SHIP
-            self.print_board()
-
-        elif self.alignment == 'v':
-            # Check and see if the ship will fit on the board vertically
-            for pos in range(int(self.ship[1])):
-                # If the ship fits add the markers to the board
-                if (self.ship_y_pos - int(self.ship[1])) < BOARD_SIZE:
-                    self.board[self.ship_y_pos + pos][self.ship_x_pos] = VERTICAL_SHIP
-                    self.display_board()
-                # If the ship does not fit on the board ask the player to try again.
-                else:
-                    print("The {} takes up {} spaces. There are only {} spaces available from position {}"
-                        .format( self.ship[0], self.ship[1], (BOARD_SIZE - self.ship_y_pos), self.alignment))
-                    print("Please try again")
-                    self.ship_position()
-        else:
-            self.ship_alignment()
-
-
-    def place_ship(self):
-        self.display_board()
-        print("\n")
-        print("Place your ship by first selecting a Row and then a Column, for example 3C or 7F.")
-        print("\n")
-        for self.ship in SHIP_INFO:
-            self.ship_position()
-            self.ship_alignment()
-
-
-
-
-
-player1_board = Board()
-player1_board.place_ship()
+#
+# player1_board = Board()
+# player1_board.place_ship()
 
 
 # print('-' * 20)
--- Project 2 Battleship/ship.py
+++ Project 2 Battleship/ship.py
@@ -1,78 +1,104 @@
-from board import *
-
-SHIP_INFO = [
-    ("Aircraft Carrier", 5),
-    ("Battleship", 4),
-    ("Submarine", 3),
-    ("Cruiser", 3),
-    ("Patrol Boat", 2)
-]
+from board import Board, SHIP_INFO, BOARD_SIZE, HORIZONTAL_SHIP
 
 
-class Ship():
-
-    def place_ship(self):
-
-        self.player_board = self.board
-
-        for ship in SHIP_INFO:
-            self.ship_count = 0
-            while self.ship_count < len(SHIP_INFO):
-                self.ship_input = (
-                (input("Place the location of the {} ({} spaces): ".format(ship[0], ship[1]))).strip()).lower()
-                self.ship_location()
-                self.ship_alignment()
-                if self.alignment == 'h':
-                    # Check and see if the ship will fit on the board
-                    for pos in range(int(ship[1])):
-                        # If the ship fits add the markers to the board
-                        if (self.ship_x_pos + int(ship[1])) < BOARD_SIZE:
-                            self.player_board[self.ship_y_pos][self.ship_x_pos + self.pos] = HORIZONTAL_SHIP
-                            self.ship_count += 1
-                        # If the ship does not fit on the board ask the player to try again.
-                        else:
-                            print(
-                                "The {} takes up {} spaces. There are only {} spaces available from position {}".format(
-                                    ship[0],
-                                    ship[1], (
-                                        BOARD_SIZE - self.ship_x_pos),
-                                    self.alignment))
-                            print("Please try again")
-                            self.place_ship()
+from battleship import *
 
 
-    def ship_location(self):
-                self.ship_loc = list(self.ship_input)
-                if len(self.ship_loc) == 2:
-                    self.ship_x_pos = ord(self.ship_loc[1]) - 97
-                    self.ship_y_pos = int(self.ship_input[0]) - 1
-                else:
-                    self.ship_x_pos = ord(self.ship_loc[2]) - 97
-                    self.ship_y_pos = int(self.ship_input[0] + self.ship_input[1]) - 1
+
+class Ship:
+
+# -- SETOUT PLAYER BOARD --
+
+    # Player input for ship position
+    def ship_position(self):
+        self.ship_input = (
+        (input("Where would you like to place the {}? The {} takes up {} spaces: ".format(
+            self.ship[0],
+            self.ship[0],
+            self.ship[1]
+            ))).strip()).lower()
+        self.ship_location = list(self.ship_input)
+        if len(self.ship_location) == 2:
+            self.ship_x_pos = ord(self.ship_location[1]) - 97
+            self.ship_y_pos = int(self.ship_input[0]) - 1
+        elif len(self.ship_location) == 3:
+            self.ship_x_pos = ord(self.ship_location[2]) - 97
+            self.ship_y_pos = int(self.ship_input[0] + self.ship_input[1]) - 1
+        else:
+            self.ship_position()
+        self.player_input_check()
 
 
+    # Check and see if the position is on the board
+    def player_input_check(self):
+        if chr(self.ship_x_pos + 97) not in ("abcdefghij") or (self.ship_y_pos + 1) > 10:
+            print("Try Again")
+            self.ship_position()
+        else:
+            pass
+
+    # Check and see if the ship hits an existing ship
+    def hits_existing_ship(self):
+        self.ship_setout = []
+        for self.pos in range(int(self.ship[1])):
+            self.ship_setout.append(self.board.board[self.ship_y_pos][self.ship_x_pos + self.pos])
+        print(self.ship_setout)
+
+        if HORIZONTAL_SHIP in self.ship_setout:
+            print ("There is an existing ship in the way. Try again")
+            self.ship_position()
+            self.ship_alignment()
+        else:
+            for self.pos in range(int(self.ship[1])):
+                self.board.board[self.ship_y_pos][self.ship_x_pos + self.pos] = HORIZONTAL_SHIP
+
+    # Player input ship alignment
     def ship_alignment(self):
-        self.alignment = (input("[h] to place it horizontal, or [v] to place it vertical: ")).lower()
 
-
-    def vertical_alignment(self):
-        # if they want to place the ship horizontally.
+        self.alignment = (input("Press [h] to place it horizontal, or [v] to place it vertical: ")).lower()
         if self.alignment == 'h':
-            # Check and see if the ship will fit on the board
-            for pos in range(int(ship[1])):
+            # Check and see if the ship will fit on the board horizontally
+            if (self.ship_x_pos + int(self.ship[1])) < BOARD_SIZE:
+                # Check and see if the ship hits an existing ship
+                self.hits_existing_ship()
+            else:
+                print(
+                    "The {} takes up {} spaces. There are only {} spaces available to the right of position {}"
+                    .format(self.ship[0], self.ship[1], (BOARD_SIZE - self.ship_x_pos), self.ship_input))
+                print("Please try again")
+                self.ship_position()
+            self.board.print_board_heading()
+            self.board.print_board()
+        elif self.alignment == 'v':
+            # Check and see if the ship will fit on the board vertically
+            for self.pos in range(int(self.ship[1])):
                 # If the ship fits add the markers to the board
-                if (self.ship_x_pos + int(ship[1])) < Board.BOARD_SIZE:
-                    self.player_board[self.ship_y_pos][self.ship_x_pos + pos] = HORIZONTAL_SHIP
-                    self.ship_count += 1
-                # If the ship does not fit on the board ask the player to try again.
+                if (self.ship_y_pos - int(self.ship[1])) < BOARD_SIZE:
+                    self.hits_existing_ship()
+                    #self.board[self.ship_y_pos - self.pos][self.ship_x_pos] = VERTICAL_SHIP
                 else:
-                    print("The {} takes up {} spaces. There are only {} spaces available from position {}".format(ship[0],
-                                                                                                                  ship[1], (
-                                                                                                                  Board.BOARD_SIZE - self.ship_x_pos),
-                                                                                                                  self.alignment))
+                    print(
+                        "The {} takes up {} spaces. There are only {} spaces available above the position of {}"
+                        .format(self.ship[0], self.ship[1], (BOARD_SIZE - self.ship_y_pos), self.ship_input))
                     print("Please try again")
+                    self.ship_position()
+            self.board.print_board_heading()
+            self.board.print_board()
+        else:
+            self.ship_alignment()
+#
+#
+    def place_ship(self):
+        self.board = Board()
+        self.board.display_board()
+        print("\n")
+        print("Place your ship by first selecting a Row and then a Column, for example 3C or 7F.")
+        print("\n")
+        for self.ship in SHIP_INFO:
+            self.ship_position()
+            self.ship_alignment()
 
-                    break
+
 
 test = Ship()
 test.place_ship()
